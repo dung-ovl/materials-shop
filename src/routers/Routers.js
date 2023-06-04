c045e28814a774be7a8379509a51f10fd37da336
UIT-20521205 <dinhiendung1026@gmail.com>
2023-06-05 01:15:29 +0700

    products pages

--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -4,9 +4,9 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 import Home from '../pages/Home'
 // import Cart from '../pages/Cart'
 // import Checkout from '../pages/Checkout'
-// import ProductDetails from '../pages/ProductDetails'
+import ProductDetails from '../pages/ProductDetails'
 // import Account from '../pages/Account'
-// import Products from '../pages/Products'
+import Products from '../pages/Products'
 // import About from '../pages/About'
 // import Contact from '../pages/Contact'
 // import Location from '../pages/Location'
@@ -23,11 +23,11 @@ function Routers() {
       <Route path='/' element={<Navigate to='home' />} />
       <Route path='home' element={<Home />} />
       {/* <Route path='about' element={<About />} />
-      <Route path='cart' element={<Cart />} />
+      <Route path='cart' element={<Cart />} /> */}
       <Route path='products' element={<Products />} />
-      <Route path='checkout' element={<Checkout />} />
+      {/* <Route path='checkout' element={<Checkout />} /> */}
       <Route path='products/:id' element={<ProductDetails />} />
-      <Route path='account' element={<Account />} />
+      {/* <Route path='account' element={<Account />} />
       <Route path='contact' element={<Contact />} />
       <Route path='project' element={<Location />} />
       <Route path='project/:id' element={<LocationDetail />} /> */}
