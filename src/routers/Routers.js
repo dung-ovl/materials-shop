793082fb4957e58ab3db8cc9d7e5814133bfbdcd
UIT-20521205 <dinhiendung1026@gmail.com>
2023-06-05 20:00:10 +0700

    Update Routers.js

--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -7,7 +7,7 @@ import Home from '../pages/Home'
 import ProductDetails from '../pages/ProductDetails'
 // import Account from '../pages/Account'
 import Products from '../pages/Products'
-// import About from '../pages/About'
+import About from '../pages/About'
 import Contact from '../pages/Contact'
 import Project from '../pages/Project'
 import ProjectDetail from '../pages/ProjectDetail'
@@ -22,8 +22,8 @@ function Routers() {
     <Routes>
       <Route path='/' element={<Navigate to='home' />} />
       <Route path='home' element={<Home />} />
-      {/* <Route path='about' element={<About />} />
-      <Route path='cart' element={<Cart />} /> */}
+      <Route path='about' element={<About />} />
+      {/* <Route path='cart' element={<Cart />} /> */}
       <Route path='products' element={<Products />} />
       {/* <Route path='checkout' element={<Checkout />} /> */}
       <Route path='products/:id' element={<ProductDetails />} />
